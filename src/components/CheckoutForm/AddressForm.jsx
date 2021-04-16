c6a0a92f457d0dfd27d92e0ab6a627571a20f776
DocErnstz <ernesto800@outlook.com>
2021-04-16 16:40:01 -0300

    address form

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
-const AddressForm = () => {
-  return (
-      <div>asd</div>
-  )
+import { commerce } from '../lib/commerce';
+import FormInput from './CustomTextField';
+
+const AddressForm = ({ checkoutToken }) => {
+ 
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+    const [shippingSubdivision, setShippingSubdivision] = useState('');
+    const [shippingOptions, setShippingOptions] = useState([]);
+    const [shippingOption, setShippingOption] = useState('');
+    const methods = useForm();
+    
+   
+   
+    return (
+      <>
+        <Typography variant="h6" gutterBottom>Shipping Adress</Typography>
+    <FormProvider {...methods}>
+        <form onSubmit="">
+            <Grid container spacing={3}>
+              <FormInput required name="firstName" label="First name" />
+               <FormInput required name="lastName" label="Last name" />
+               <FormInput required name="address1" label="Address line 1" />
+               <FormInput required name="email" label="Email" />
+               <FormInput required name="city" label="City" />
+                <FormInput required name="zip" label="Zip / Postal code" />
+            </Grid>
+        </form> 
+    </FormProvider>
+        
+
+        
+        
+       
+      </>
+    );
 };
 
 export default AddressForm;
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -3,46 +3,82 @@ import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgr
 import { Link, useHistory } from 'react-router-dom';
 
 import { commerce } from '../../lib/commerce';
-import useStyles from './styles';
-
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
+import useStyles from './styles';
 
 const steps = ['Shipping address', 'Payment details'];
 
+const Checkout = ({ cart }) => {
+  const [checkoutToken, setCheckoutToken] = useState(null);
+  const [activeStep, setActiveStep] = useState(0);
+  const [shippingData, setShippingData] = useState({});
+  const classes = useStyles();
+  const history = useHistory();
 
-const Checkout = ({ cart}) => {
-    const [checkoutToken, setCheckoutToken] = useState(null);
-    const [activeStep, setActiveStep] = useState(0);
-    const [shippingData, setShippingData] = useState({});
-    const classes = useStyles();
-    const history = useHistory();
-    const Form = () => (activeStep === 0 ? <AddressForm  /> : <PaymentForm />);
-    
-    const Confirmation = () => (
-        <div>Confirmation</div>
-    );
-    return (
-      <>
-        <CssBaseline />
-        <div className={classes.toolbar} />
-        <main className={classes.layout}>
-          <Paper className={classes.paper}>
-            <Typography variant="h4" align="center">Checkout</Typography>
-            <Stepper activeStep={activeStep} className={classes.stepper}>
-              {steps.map((label) => (
-                <Step key={label}>
-                  <StepLabel>{label}</StepLabel>
-                </Step>
-              ))}
-            </Stepper>
-            {activeStep === steps.length ? <Confirmation /> : <Form />}
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
 
-          </Paper>
-        </main>
-      </>
-    );
+  useEffect(() => {
+    if (cart.id) {
+      const generateToken = async () => {
+        try {
+          const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
+         
+
+          setCheckoutToken(token);
+          
+          
+        } catch {
+          console.log("a");
+          
+        }
+      };
+   
+      generateToken();
+     
+    }
+  }, [cart]);
+ 
+  console.log(checkoutToken.id);
+
+  const test = (data) => {
+    setShippingData(data);
+
+    nextStep();
   };
 
+  const Confirmation = () => (
+      <div>Confirmation</div>
+  );
+
+  
+
+
+
+  const Form = () => (activeStep === 0
+    ? <AddressForm checkoutToken={checkoutToken} />
+    : <PaymentForm  />);
+
+  return (
+    <>
+      <CssBaseline />
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">Checkout</Typography>
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
+        </Paper>
+      </main>
+    </>
+  );
+};
 
 export default Checkout;
